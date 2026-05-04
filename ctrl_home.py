4c31316cabc8a3fea80cbcf4b9d5bb13ce1f0f4f
kathleen208 <kathleensoares310@gmail.com>
2026-05-04 10:23:25 -0300

    desmenbrando template

--- ctrl_home.py
+++ ctrl_home.py
@@ -13,15 +13,15 @@ bp = Blueprint(__name__, "HomeController")
 @bp.route("/") # cria uma rota
 def index(): # função que gerencia rota
     """ Página inicial"""
-    if '_user_id' not in session:
-         return redirect(url_for("auth.login"))
-    
+    if  '_user_id'not in session:
+        return redirect(url_for("auth.login"))
+   
     return render_template("dashboard/index.html") # Renderiza um template
 
-@bp.route("/dashboard")  # cria uma rota
-def dashboard(): # função que gerencia rota
-    """ Painel de Vendas """
+@bp.route("/dashboard") # cria uma rota para navegador http://127.0.0.1:5000/dashboard
+def dashboard(): # função que gerencia rota deve ser única
+    """ Painel de Vendas"""
     # if 'user' not in session:
-    #      return redirect(url_for("auth.login"))
-
-    return render_template("dashboard/index.html", title="Painel de Vendas")  # Renderiza um template
+    #     return redirect(url_for("auth.login"))
+   
+    return render_template("dashboard/index.html", title = "Painel de Vendas") # Renderiza um template
